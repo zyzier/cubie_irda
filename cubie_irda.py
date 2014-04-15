581f04358e9d870d1a974e0e68ce35c806e0f3d9
peps <pepsbox@gmail.com>
2014-04-15 14:43:50 +0400

    adding comments

--- cubie_irda.py
+++ cubie_irda.py
@@ -1,12 +1,17 @@
+#encoding utf-8
+''' Main script for cubie remote control
+    For now it using for manage mpd server '''
 import string
 import os
 
 from evdev import InputDevice
 from select import select
 
+#Device file
 dev = InputDevice('/dev/input/event0')
 
 def scan(device):
+  '''Infinity loop that checks the device file'''
     while True:
        r,w,x = select([device], [], [])
        for event in device.read():
